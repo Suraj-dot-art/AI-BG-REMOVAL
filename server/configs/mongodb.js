e0e7c4c53f3fe9de5e7930cd809090e3abc3a85f
Suraj <surajdivekarsd27@gmail.com>
2024-12-10 00:29:11 +0530

    changed mongoURI

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,29 +1,41 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        // Set up event listeners before connecting
-        mongoose.connection.on('connected', () => {
-            console.log("Database Connected Successfully");
-        });
 
-        mongoose.connection.on('error', (err) => {
-            console.error("Database connection error:", err);
-        });
+    mongoose.connection.on('connected' , () => {
+        console.log("Database Connected Succesfully");
+    })
 
-        mongoose.connection.on('disconnected', () => {
-            console.log("Database disconnected");
-        });
+    await mongoose.connect(`${process.env.MONGODB_URI}/ai-bg-removal`)
+}
+
+
+
+
+// const connectDB = async () => {
+//     try {
+//         // Set up event listeners before connecting
+//         mongoose.connection.on('connected', () => {
+//             console.log("Database Connected Successfully");
+//         });
+
+//         mongoose.connection.on('error', (err) => {
+//             console.error("Database connection error:", err);
+//         });
+
+//         mongoose.connection.on('disconnected', () => {
+//             console.log("Database disconnected");
+//         });
     
-        // Connect to the database
-        // console.log(process.env.MONGODB_URI);
-        await mongoose.connect(`${process.env.MONGODB_URI}/aiBgRem`);
+//         // Connect to the database
+//         // console.log(process.env.MONGODB_URI);
+//         await mongoose.connect(`${process.env.MONGODB_URI}`);
 
-    } catch (err) {
-        console.error("Error connecting to the database:", err);
-        process.exit(1); // Exit process with failure
-    }
-};
+//     } catch (err) {
+//         console.error("Error connecting to the database:", err);
+//         process.exit(1); // Exit process with failure
+//     }
+// };
 
 
  export default connectDB;
