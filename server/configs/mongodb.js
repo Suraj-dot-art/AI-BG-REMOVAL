cefaafb32ba9a53573d11da9fb70677d8b8e4e10
Suraj <surajdivekarsd27@gmail.com>
2025-01-17 19:03:05 +0530

    changed monogFile

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(`${process.env.MONGODB_URI}/ai-bg-removal`);
         console.log(`Database Connected Successfully: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Database Connection Error: ${error.message}`);
